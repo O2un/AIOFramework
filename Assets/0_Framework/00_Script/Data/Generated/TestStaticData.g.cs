25bf97ed1671624a26d2b571e4afd6bc5fbee97b
조재원 <91.jaewon@gmail.com>
2026-05-17 21:57:26 +0900

    폴더 네이밍 수정

---
+++ Assets/0_Framework/00_Script/Data/Generated/TestStaticData.g.cs
@@ -0,0 +1,10 @@
+﻿using O2un.Roslyn.Generator;
+namespace O2un.Data
+{
+    [O2un.Roslyn.Generator.StaticData]
+    public partial class TestStaticData
+    {
+        public int num {get; init;}
+        public string str {get; init;}
+    }
+}
--- Assets/0_Framework/00_Script/__DEV/TestMono.cs
+++ Assets/0_Framework/00_Script/__DEV/TestMono.cs
@@ -5,6 +5,7 @@ using O2un;
 using O2un.Core;
 using O2un.Core.Network;
 using O2un.Core.Utils;
+using R3;
 using UnityEngine;
 
 namespace O2un.DEV 
@@ -12,10 +13,10 @@ namespace O2un.DEV
 #if UNITY_EDITOR
     public class TestMono : SafeMono
     {
+            private readonly Subject<Unit> _moveEndSubject = new();
         [TestButton]
         public void LoadScene()
         {
-            
         }
     }
 #endif
